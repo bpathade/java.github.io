9f2f5a55cb7f79097bb3f810f2fdd90447747c7d
bpathade <92673871+bpathade@users.noreply.github.com>
2022-06-26 13:51:52 +0530

    Demo1.java

---
+++ Demo1.java
@@ -0,0 +1,18 @@
+//Java Program to illustrate how to define a class and fields
+package com.hello;
+
+public class Demo1 {
+	//defining fields
+	int id;//instance variable
+	String name;
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+Demo1 d1=new Demo1();//creating an object of a Demo1 class
+System.out.println(d1.id=100);//Accessing  member through reference variable
+System.out.println(d1.name="Bhagwan");//printing values of an object
+	}
+
+}
+# java.github.io
